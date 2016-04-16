46df88fb418b02797e071866121497b6c27674f4
Tamir Sagi <tamirsagi@gmail.com>
2016-04-16 16:27:14 +0300

    added sensor configuration. handled tachometer interupt

--- Pegasus_Hardware_Unit.ino
+++ Pegasus_Hardware_Unit.ino
@@ -21,15 +21,16 @@ the system is waiting for handshake and sends echo every 5 seconds until handsha
 const int tachometer_interupt_port = 18;
 volatile boolean revolution_occured = false;
 unsigned int revolution_count = 0;
-unsigned long current_time = 0;
-unsigned long previous_time = 0;
+unsigned long tachometer_current_time = 0;
+unsigned long tachometer_previous_time = 0;
 unsigned int rps = 0;
 const int wheelSlots = 5;
+int lastValue = -1;
 
 
 ////////////// Ultra Sonic Sensors \\\\\\\\\\\\\\
 
-const int  MAX_DISTANCE = 20;
+const int  MAX_DISTANCE = 60;
 const int NUMBER_OF_ULTRA_SONIC_SENSORS = 7;
 const int sensors_ids[NUMBER_OF_ULTRA_SONIC_SENSORS] = { 1, 2, 3, 4, 5, 6, 7 };
 const int sensors_triger_digital_pins[NUMBER_OF_ULTRA_SONIC_SENSORS] = { 34, 36, 46, 44, 42, 40, 38 };
@@ -43,7 +44,10 @@ NewPing sonar[NUMBER_OF_ULTRA_SONIC_SENSORS] = { NewPing(sensors_triger_digital_
 											     NewPing(sensors_triger_digital_pins[6], sensors_echo_digital_pins[6], MAX_DISTANCE) 
 											  };
 bool sensorStatesArray[NUMBER_OF_ULTRA_SONIC_SENSORS] = { false, false, false, false, false, false, false };
+int sensors_last_values[NUMBER_OF_ULTRA_SONIC_SENSORS] = { -1, -1, -1, -1, -1, -1, -1 };
 unsigned int uS = 0;
+unsigned long us_sensors_current_time = 0;
+unsigned long us_sensors_previous_time = 0;
 
 /////////////// Motors \\\\\\\\\\\\\\\\\\\\\
 
@@ -55,15 +59,20 @@ Servo mSteerMotor;
 /*
 Messages And protocol Abberviations
 */
-const char *KEY_MESSAGE_TYPE = "MT";				 // MT = Message Type
-const int VALUE_MESSAGE_TYPE_INFO = 1000;			 //INFO
-const int VALUE_MESSAGE_TYPE_ACTION = 2000;			 //ACTION
-const char *KEY_INFO_TYPE = "IT";					 //IT = Info Type key
 
 const char START_MESSAGE = '$';						//Begining message 
 const char END_MESSAGE = '#';						//end of message 
 const char MESSAGE_SAPERATOR = ',';				   //message saparator , part of the messaging protocol
 const char MESSAGE_KEY_VALUE_SAPERATOR = ':';
+
+
+const char *KEY_MESSAGE_TYPE = "MT";				 // MT = Message Type
+const int VALUE_MESSAGE_TYPE_INFO = 1000;			 //INFO
+const int VALUE_MESSAGE_TYPE_ACTION = 2000;			 //ACTION
+const int VALUE_MESSAGE_TYPE_SETTINGS = 3000;		//Settings
+const int VALUE_MESSAGE_TYPE_ERROR = 4000;			 //ERROR
+const char *KEY_INFO_TYPE = "IT";					 //IT = Info Type key
+
 
 /*
 Incoming
@@ -73,6 +82,9 @@ const char *KEY_DIGITAL_SPEED = "DS";				//Digital Speed Key
 const char *KEY_ROTATION_ANGLE = "RA";				//Rotation Angle key
 const char *KEY_STEERING_DIRECTION = "SD";			// Steering Direction key
 const char *KEY_DRIVING_DIRECTION = "DD";			//Driving Direction key
+
+const char *KEY_SETTINGS_ACTION_TYPE = "ST";		//Settings Type key
+const char *KEY_PREFIX_ULTRA_SONIC_SENSOR = "S";		//Settings Type key
 
 const String VALUE_STEERING_NONE = "N";				//N = None, no steering
 const String VALUE_STEERING_RIGHT = "R";			//R = Right,  Right Steering
@@ -95,9 +107,6 @@ const String VALUE_STATUS = "STATUS";
 const String VALUE_SENSOR_DATA = "SENSOR_DATA";			
 const char* KEY_SENSOR_DATA = "SD";						//SD = Sensor Data
 const char* KEY_STATUS = "ST";						  //ST = STATUS
-//const String KEY_SERIAL_PORT = "SP";				 //SP = Serial Port
-//const String KEY_STEER_MOTOR = "SM";				//SM = Steering Motor
-//const String KEY_BACK_MOTOR = "BM";				//BM = Back Motor
 
 const String VALUE_OK = "OK";
 const String VALUE_ERROR = "ER";
@@ -107,18 +116,24 @@ const String VALUE_ERROR = "ER";
 
 /////////////// ACTIONS \\\\\\\\\\\\\\\\\\\\\\\
 /* Action For Arduino */
-const int ACTION_DRIVING_DIRECTION = 0;
-const int ACTION_BACK_MOTOR = 1;
-const int ACTION_STEER_MOTOR = 2;
-const int ACTION_CHANGE_SENSOR_STATE = 3;
+const int ACTION_DRIVING_DIRECTION = 2001;
+const int ACTION_BACK_MOTOR = 2002;
+const int ACTION_STEER_MOTOR = 2003;
+const int ACTION_CHANGE_SENSOR_STATE = 2004;
+
 
 /////////////// INFO \\\\\\\\\\\\\\\\\\
 
 const int VALUE_HARDWARE_STATUS_READY = 100;
 const int VALUE_LOGIC_UNIT_READY = 200;
 
+/////////////SETTINGS \\\\\\\\\\\\\\\\\\\\\
+
+const int SETTINGS_SET_SENSORS = 3001;
+
+
 //////////////////////////////////////////////////////////////////////////////////////////////////////
-const int UPDATE_INTERVAL = 60 * 1000 * 3;		   // 5 minutes update interval
+const int UPDATE_INTERVAL = 60 * 1000 * 2;		   // 2 minutes update interval
 const int MIN_DIGITAL_SPEED = 0;				  //MIN digital speed value
 const int MAX_DIGITAL_SPEED = 255;				 //MAx digital speed value
 const int STRAIGHT_STEER_ANGLE = 90;			//MAX steer angle value
@@ -126,22 +141,24 @@ const int MIN_STEER_ANGLE = 50;					//MIN steer angle value
 const int MAX_STEER_ANGLE = 130;				//MAX steer angle value
 
 String mInputMessage = "";						//keeps incoming messages from Raspberry Pi
-boolean mReceivedEntireMessage = false;			//whether the string is complete
 boolean mIsLogicUnitReady = false;				//indicates whether the Raspberry pi is ready for communication
 double mCurrentteeringAngle;					//keep the current steering angle
 int mLastDigitalSpeed;							//keep last digital speed
-int mCurrentDrivingDirection;                            //Keep Last Driving direction (Forward, Backword)...
+int mCurrentDrivingDirection;                   //Keep Last Driving direction (Forward, Backword)...
 double mLastSteeringAngle;						//keep last steering angle
 String mSystemInitialStatus = "";				//keep initiate status of the system , we send this string to logic unit
 
-int elapsedTime = 0;
-int measure = 0;
-int lastMeasure = 0;
+const int TACHOMETER_INTERVAL_TIME = 1 * 200;		//Measure sensors every 0.2 sec
+const int US_INTERVAL_TIME = 1 * 500;				//Measure sensors every 0.5 sec
+
+bool isStopped = true;
 //////////////////////////////////////////////////////////////////////////////////////////////////////
 // Interrupt Service Routine (ISR)
 void revolution()
 {
-    revolution_occured = true;
+	if (!isStopped){
+		revolution_count++;
+	}
 }
 
 void setup(){
@@ -172,125 +189,21 @@ void setup(){
         + END_MESSAGE;
 
 
-	/*mSystemInitialStatus = "MT:2000, VA : 3, SID : 1, SS : 1";
-	Serial.println("Before");
-	for (int i = 0; i < 7; i++){
-		Serial.print("Sensor ");
-		Serial.print(i + 1);
-		Serial.print(sensorStatesArray[i]);
-		Serial.println();
-	}
-	sendUltraSonicSensorData();
-	handleMessage(mSystemInitialStatus);
-	Serial.println("after");
-	for (int i = 0; i < 7; i++){
-		Serial.print("Sensor ");
-		Serial.print(i + 1);
-		Serial.print(":");
-		Serial.print(sensorStatesArray[i]);
-		Serial.println();
-	}
-	sendUltraSonicSensorData();*/
+
 }
 
 void loop(){
 
-	/*
-		handle incoming messages
-	*/
-	if (mReceivedEntireMessage){
-		handleMessage(mInputMessage);
-		//clear data
-		mInputMessage = "";
-		mReceivedEntireMessage = false;
-	}
-	lastMeasure = millis();
-    /*
-        wait for handshake, we send status and wait 3 seconds
-     */
+
+	//   wait for handshake, we send status and wait 3 seconds
     if (!mIsLogicUnitReady){
         Serial.println(mSystemInitialStatus);	//send message to logic unit
         delay(5000);	//sleep 5 seconds
     }
-
-	else if (lastMeasure - measure > 1000){//system is working we send measurements every 1 sec
-			measure = lastMeasure;
+	else{		//system is working we send measurements every 1 sec
 			sendUltraSonicSensorData();
-			//sendTachometerData();
+			sendTachometerData();
 		}
-		
-
-	//// scan from 0 to 180 degrees
-	//for (int angle = 50; angle <= 130; angle++)
-	//{
-	//	mSteerMotor.write(angle);
-	//	delay(5);
-	//}
-	////// now scan back from 180 to 0 degrees
-	//for (int angle = 130; angle >= 50; angle--)
-	//{
-	//	mSteerMotor.write(angle);
-	//	delay(5);
-	//}
-	/*Serial.println("Digital Speed");
-	handleMessage(mInputMessage);
-	Serial.println("Rotation R");
-	mInputMessage = "AT:2,SD:R,RA:80.7";
-	handleMessage(mInputMessage);
-	Serial.println("Rotation L ");
-	mInputMessage = "AT:2,SD:L,RA:130";
-	handleMessage(mInputMessage);*/
-
-
-	//RMP testing
-
-	/*unsigned int time = 0, RPM = 0;
-	Serial.println("Reading RPM.....");
-	time = delay1();
-	Serial.println("Please Wait.....");
-	RPM = (time * 12) / 5;
-	Serial.println("RPM=");
-	Serial.println(RPM);
-	delay(2000);*/
-	//if (revolution_occured) {
-	//	revolution_count++;
-	//	revolution_occured = !revolution_occured;
-	//}
-
-	//current_time = millis();
-	//if (current_time - previous_time >= 1000) {
-	//	rpm = (revolution_count / 5);
-	//	Serial.print("rpm = ");
-	//	Serial.println(rpm);
-	//	//Serial.print ("counts = ");
-	//	//Serial.println (revolution_count); 
-	//	previous_time = current_time;
-	//	revolution_count = 0;
-	//}
-
-	///////Sonar testing
-
-	//delay(50);
-
-	//for (int i = 0; i < NUMBER_OF_ULTRA_SONIC_SENSORS; i++){
-
-	//	int uS = sonar[i].ping();
-	//	Serial.println(sensors_ids[i]);
-	//	Serial.print("Ping: ");
-	//	Serial.print(uS / US_ROUNDTRIP_CM);
-	//	Serial.println("cm");
-	//	delay(500);
-	//}
-	//
-
-	/*
-	Serial.println("SONAR 2!!!!!!!!!!!!");
-	uS = sonar[1].ping();
-	Serial.println("SONAR 1!!!!!!!!!!!!");
-	Serial.print("Ping: ");
-	Serial.print(uS / US_ROUNDTRIP_CM);
-	Serial.println("cm");*/
-
 }
 
 
@@ -308,11 +221,14 @@ void serialEvent(){
             temp[0] = Serial.read();
             if (mInputMessage.length() == 0 && temp[0] == START_MESSAGE)
                 procceed = true;
-
             else if (procceed){
                 //check if its the end of the message
                 if (temp[0] == END_MESSAGE && mInputMessage.length() > 0){
-                    mReceivedEntireMessage = true;
+					Serial.println("message received from pi start");
+					Serial.println(mInputMessage);
+					Serial.println("finish");
+					handleMessage(mInputMessage);
+					mInputMessage = "";
                     procceed = false;
                 }
                 else
@@ -342,8 +258,7 @@ String systemCheckUp(){
 * The Message is in format MessageType(Vehicle_Action, Info,....), params,params...
 */
 void handleMessage(String msgFromRaspberry){
-	Serial.println(msgFromRaspberry);
-    if (!isNullOrEmpty(msgFromRaspberry)){
+	if (!isNullOrEmpty(msgFromRaspberry)){
         int len = msgFromRaspberry.length() + 1;						// + 1 to keep eos '\0' char
         char* msgToHandle = new char[len];
         msgFromRaspberry.toCharArray(msgToHandle, len);					//convert the String to char[] in order to use strtok
@@ -351,16 +266,22 @@ void handleMessage(String msgFromRaspberry){
         if (!isNullOrEmpty(messageType) && isNumber(messageType)){
             int messageTypeValue = messageType.toInt();					//return Message type
             switch (messageTypeValue){
-            case  VALUE_MESSAGE_TYPE_INFO:
-            {
-              handleInfoMessage(msgToHandle);
-              break;
-            }
-            case VALUE_MESSAGE_TYPE_ACTION:
-            {
-               handleAction(msgToHandle);
-               break;
-            }
+				case  VALUE_MESSAGE_TYPE_INFO:
+				{
+				  handleInfoMessage(msgToHandle);
+				  break;
+				}
+				case VALUE_MESSAGE_TYPE_ACTION:
+				{
+				   handleAction(msgToHandle);
+				   break;
+				}
+				case VALUE_MESSAGE_TYPE_SETTINGS:
+					handleSettings(msgToHandle);
+					break;
+				case VALUE_MESSAGE_TYPE_ERROR:
+
+					break;
 
             }//end of switch
 
@@ -437,11 +358,23 @@ void handleAction(char* msgToHandle){
 				break;
 			}
 			case ACTION_CHANGE_SENSOR_STATE:
-				Serial.println("here");
 				handleSensorStateChanged(msgToHandle);
 				break;
         }
     }
+
+}
+
+void handleSettings(char* msgToHandle){
+	String actionType = getValue(msgToHandle, KEY_SETTINGS_ACTION_TYPE);	//get Settings
+	if (!isNullOrEmpty(actionType) && isNumber(actionType)) {
+		int actionTypeValue = actionType.toInt();							//return Action type
+		switch (actionTypeValue){
+		case SETTINGS_SET_SENSORS:
+			setSensors(msgToHandle);
+			break;
+		}
+	}
 
 }
 
@@ -481,6 +414,24 @@ boolean isNumber(String msg){
     }
 }
 
+/*
+set sensor max distance
+*/
+void setSensors(char* sensorData){
+	for (int i = 0; i < NUMBER_OF_ULTRA_SONIC_SENSORS; i++){
+		String key = String(KEY_PREFIX_ULTRA_SONIC_SENSOR) + (i + 1);
+		int len = key.length() + 1;											// + 1 to keep eos '\0' char
+		char* relevantKEy = new char[len];
+		key.toCharArray(relevantKEy, len);									//convert the String to char[] in order to use strtok
+		String sensorMaxDistance = getValue(sensorData, relevantKEy);		//get Value
+		if (!isNullOrEmpty(sensorMaxDistance) && isNumber(sensorMaxDistance)){
+			int maxDistance = sensorMaxDistance.toInt();
+			sonar[i] = NewPing(sensors_triger_digital_pins[i], sensors_echo_digital_pins[i], maxDistance);
+		}
+		delete[] relevantKEy;
+	}
+}
+
 
 
 /**
@@ -488,8 +439,7 @@ boolean isNumber(String msg){
 */
 void handleBackMotor(char * msgToHandle){
     String sd = getValue(msgToHandle, KEY_DIGITAL_SPEED);
-    if (!isNullOrEmpty(sd) && isNumber(sd))
-    {
+    if (!isNullOrEmpty(sd) && isNumber(sd)){
         int digitalSpeed = sd.toInt();
         if (MIN_DIGITAL_SPEED <= digitalSpeed && digitalSpeed <= MAX_DIGITAL_SPEED){
             mLastDigitalSpeed = digitalSpeed;
@@ -515,6 +465,7 @@ void handleSteerMotor(char * msgToHandle){
             char* ra = new char[len + 1];
             rotationAngle.toCharArray(ra, len + 1);
             double angle = atof(ra);
+			mLastSteeringAngle = angle;
             delete[] ra;
             if (direction.compareTo(VALUE_STEERING_RIGHT) == 0)
                 turnSteeringRight(angle);
@@ -531,13 +482,10 @@ Method handles direction changing
 void handleDirections(char *msgToHandle){
     String direction = getValue(msgToHandle, KEY_DRIVING_DIRECTION);
     mBackMotor.run(RELEASE);							//reset back motor
-    if (direction.compareTo(VALUE_DRIVING_FORWARD) == 0)
-    {
+    if (direction.compareTo(VALUE_DRIVING_FORWARD) == 0) {
         mCurrentDrivingDirection = FORWARD;
         mBackMotor.run(FORWARD);
-    }
-    else if (direction.compareTo(VALUE_DRIVING_BACKWARD) == 0)
-    {
+    }else if (direction.compareTo(VALUE_DRIVING_BACKWARD) == 0){
         mCurrentDrivingDirection = BACKWARD;
         mBackMotor.run(BACKWARD);
     }
@@ -561,18 +509,25 @@ void handleSensorStateChanged(char* msgToHandle){
 	Message send sensor Data
 */
 void sendUltraSonicSensorData(){
-	for (int i = 0; i < NUMBER_OF_ULTRA_SONIC_SENSORS; i++){
-		if (sensorStatesArray[i]){	//sensor is enable and should send data
-			int uS = sonar[i].ping();
-			int dist = uS / US_ROUNDTRIP_CM;
-			String sensorDataMsg = START_MESSAGE
-				+ String(KEY_MESSAGE_TYPE) + MESSAGE_KEY_VALUE_SAPERATOR + VALUE_MESSAGE_TYPE_INFO + MESSAGE_SAPERATOR
-				+ String(KEY_INFO_TYPE) + MESSAGE_KEY_VALUE_SAPERATOR + VALUE_SENSOR_DATA + MESSAGE_SAPERATOR
-				+ String(KEY_SENSOR_ID) + MESSAGE_KEY_VALUE_SAPERATOR + sensors_ids[i] + MESSAGE_SAPERATOR
-				+ String(KEY_SENSOR_DATA) + MESSAGE_KEY_VALUE_SAPERATOR + dist
-				+ END_MESSAGE;
-			Serial.println(sensorDataMsg);
+	us_sensors_current_time = tachometer_current_time = millis();
+	if (us_sensors_current_time - us_sensors_previous_time >= US_INTERVAL_TIME){
+		for (int i = 0; i < NUMBER_OF_ULTRA_SONIC_SENSORS; i++){
+			if (sensorStatesArray[i]){	//sensor is enable and should send data
+				int uS = sonar[i].ping();
+				int dist = uS / US_ROUNDTRIP_CM;
+				if (sensors_last_values[i] != dist){
+					sensors_last_values[i] = dist;
+					String sensorDataMsg = START_MESSAGE
+						+ String(KEY_MESSAGE_TYPE) + MESSAGE_KEY_VALUE_SAPERATOR + VALUE_MESSAGE_TYPE_INFO + MESSAGE_SAPERATOR
+						+ String(KEY_INFO_TYPE) + MESSAGE_KEY_VALUE_SAPERATOR + VALUE_SENSOR_DATA + MESSAGE_SAPERATOR
+						+ String(KEY_SENSOR_ID) + MESSAGE_KEY_VALUE_SAPERATOR + sensors_ids[i] + MESSAGE_SAPERATOR
+						+ String(KEY_SENSOR_DATA) + MESSAGE_KEY_VALUE_SAPERATOR + dist
+						+ END_MESSAGE;
+					Serial.println(sensorDataMsg);
+				}
+			}
 		}
+		us_sensors_previous_time = us_sensors_current_time;
 	}
 }
 
@@ -580,12 +535,8 @@ void sendUltraSonicSensorData(){
 	mesaure RPM and send data
 */
 void sendTachometerData(){
-	if (revolution_occured) {
-		revolution_count++;
-		revolution_occured = !revolution_occured;
-	}
-	current_time = millis();
-	if (current_time - previous_time >= 1000) {
+	tachometer_current_time = millis();
+	if (tachometer_current_time - tachometer_previous_time >= TACHOMETER_INTERVAL_TIME && revolution_count > 0) {
 		rps = (revolution_count / wheelSlots);		
 		String sensorDataMsg = START_MESSAGE
 			+ String(KEY_MESSAGE_TYPE) + MESSAGE_KEY_VALUE_SAPERATOR + VALUE_MESSAGE_TYPE_INFO + MESSAGE_SAPERATOR
@@ -594,7 +545,7 @@ void sendTachometerData(){
 			+ String(KEY_SENSOR_DATA) + MESSAGE_KEY_VALUE_SAPERATOR + rps
 			+ END_MESSAGE;
 		Serial.println(sensorDataMsg);
-		previous_time = current_time;
+		tachometer_previous_time = tachometer_current_time;
 		revolution_count = 0;
 	}
 
@@ -606,13 +557,16 @@ void sendTachometerData(){
 *   Method change back dc motor speed
 */
 void changeBackMotorSpeed(int digitalSpeed){
-    if (digitalSpeed == 0)
-        mBackMotor.run(RELEASE);
+	isStopped = digitalSpeed == 0;
+	if (isStopped){
+		mBackMotor.run(RELEASE);
+	}
     else
     {
         mBackMotor.run(mCurrentDrivingDirection);
         mBackMotor.setSpeed(digitalSpeed);
     }
+	
 }
 
 
